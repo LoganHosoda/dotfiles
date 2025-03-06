136b60aee3bf7239917800111e6a6b944bf1881a
Logan Hosoda <35867188+LoganHosoda@users.noreply.github.com>
2025-03-06 14:31:38 -0800

    keybinding setup

--- lazy-nvim/.config/nvim/lua/config/keymaps.lua
+++ lazy-nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,10 +3,29 @@
 -- Add any additional keymaps here
 
 -- Exit from insert mode by pressing jj
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.keymap.set("i", "jj", "<Esc>")
+
+-- Move selected lines of code without having to delete and paste
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Yank highlighted code to the clipboard with space + y
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Delete without saving to buffer
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- Quality of life remaps
+vim.keymap.set("n", "<leader>b", "<cmd>silent :b#<CR>")
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 -- Open telescope buffers menu with added customization
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   "n",
   "<A-b>",
   "<cmd>Telescope buffers initial_mode=normal theme=ivy<cr>",
