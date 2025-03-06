d96d7428fb4ff9b5b9f676c27302e6dfe70fec5a
Logan Hosoda <35867188+LoganHosoda@users.noreply.github.com>
2025-03-06 06:59:58 -0800

    updated neovim config. backing up to try lazyvim

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -20,7 +20,10 @@ return {
   {
     "catppuccin/nvim", name = "catppuccin",
     config = function()
-    vim.cmd("colorscheme catppuccin");
+      require('catppuccin').setup({
+        transparent_background = true
+      })
+      vim.cmd("colorscheme catppuccin");
     end
   },
   -- {
